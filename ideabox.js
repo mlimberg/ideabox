dab6702316e638bb5295b6c62666f986573938ac
mlimberg <limbergmike@gmail.com>
2016-10-24 16:50:16 -0600

    built two new functions constructor function to build new idea boxes function to clear input fields upon button click

--- ideabox.js
+++ ideabox.js
@@ -1,10 +1,24 @@
 var ideaList = $('.idea-list');
-var inputFields = $('');
 var ideaTitle = $('.idea-title');
 var ideaBody = $('.idea-body');
-var saveButton = $('#save-button')
+var saveButton = $('#save-button');
+var titleField = $('#title-input');
+var bodyField = $('#body-input');
+
+
+function NewIdeaConstructor(titleText, bodyText) {
+  this.titleText = titleField.val();
+  this.bodyText = bodyField.val();
+}
+
+function clearInputFields() {
+  return titleField.val("") && bodyField.val("");
+}
 
 function addNewIdeaBox(titleText, bodyText) {
+  var currentIdea = new NewIdeaConstructor();
+  var titleText = currentIdea['titleText'];
+  var bodyText = currentIdea['bodyText'];
   var newIdea = $(
     `<article class="idea-box">
       <h1 class="idea-title">${titleText}</h1>
@@ -17,6 +31,5 @@ ideaList.prepend(newIdea);
 
 saveButton.on('click', function() {
   addNewIdeaBox();
-}
-
-)
+  clearInputFields();
+});
